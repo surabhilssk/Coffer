d576d0067049eb57fb4a93cc935b73d725aaee79
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-08-11 18:22:58 +0530

    Add Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { AppBar } from "@/components/AppBar";
+import { LandingPage } from "@/components/LandingPage";
 
 export default function Home() {
   return (
     <div>
       <AppBar />
+      <LandingPage />
     </div>
   );
 }
