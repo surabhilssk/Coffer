5ce4e7e965604cb80cedb181aa1a86514956bf3b
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-08-15 09:15:17 +0530

    Add blockType setstate

--- lib/store.ts
+++ lib/store.ts
@@ -3,13 +3,15 @@
 import { create } from "zustand";
 
 interface UseBlockProps{
-    blockType: string,
+    blockType: number,
+    setBlockType: (type: number) => void,
     setBlockSolana: () => void,
     setBlockEthereum: () => void
 }
 
 export const useBlockType = create<UseBlockProps>((set) => ({
-    blockType: "",
-    setBlockSolana: () => set((state) => ({ blockType: "solana "})),
-    setBlockEthereum: () =>  set((state) => ({ blockType: "ethereum" }))
-}))
+    blockType: 0,
+    setBlockType: (type) => set({ blockType: type}),
+    setBlockSolana: () => set({ blockType: 501}),
+    setBlockEthereum: () =>  set({ blockType: 60 })
+}));
